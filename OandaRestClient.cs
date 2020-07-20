373c9cc5010fa55195c4aa5fc120073bc2ae3b62
Alan Sebastian <alansebastian4@gmail.com>
2020-07-20 18:11:42 -0400

    Message posted

--- OandaRestClient.cs
+++ OandaRestClient.cs
@@ -1,26 +1,60 @@
 ﻿using RestSharp;
 using System;
 using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
 
 namespace testapp
 {
     public class OandaRestClient
     {
-       public void PlaceOrder(List<string> tradeData)
+        public void PlaceOrder(List<string> tradeData)
         {
-            string currencyPair = tradeData[0];
-            string takeProfit = tradeData[1];
-            string stopLoss = tradeData[2];
+            string currencyPair = "XAU_JPY";//tradeData[0];
+            string takeProfit = "185344";//tradeData[1];
+            string stopLoss = "18500";//tradeData[2];
+            string units = "1";
 
-            var jsonBody = "{\"order\": {\r\n    \"units\": \"1000\",\r\n    \"instrument\": \"" +
+            var jsonBody = "{\"order\": {\r\n    \"units\": \"" +
+                units +
+                "\",\r\n    \"instrument\": \"" +
                currencyPair +
                 "\",\r\n    \"timeInForce\": \"FOK\",\r\n    \"type\": \"MARKET\",\r\n    \"takeProfitOnFill\":{\"price\":\"" +
                 takeProfit +
                 "\"},\r\n    \"stopLossOnFill\" :{\"price\":\"" +
                 stopLoss +
                 "\"},\r\n    \"positionFill\": \"DEFAULT\"\r\n  }\r\n}";
+            var restClient = new RestClient("https://api-fxpractice.oanda.com/v3/accounts/101-002-14835452-002/orders");
+            var restRequest = new RestRequest();
+            restRequest.Method = RestSharp.Method.POST;
+
+
+            restRequest.AddHeader("Authorization", "Bearer 551cb19f4a836f950717a0202b8dde87-adad3a86ed84c81861f9b947c435220c");
+            restRequest.AddParameter("application/json", jsonBody, ParameterType.RequestBody);
+            var response = restClient.Execute(restRequest);
+            Console.WriteLine(response.StatusCode);
+
+        }
+        public void PlaceLimitOrder(List<string> tradeData)
+        {
+            string currencyPair = "XAU_JPY";//tradeData[0];
+            string limit = "185102";
+            string takeProfit = "185344";//tradeData[1];
+            string stopLoss = "18500";//tradeData[2];
+            string units = "1";
+            string expiry = DateTime.Now.AddHours(6.0).ToString();
+
+            var jsonBody = "{\n  \"order\": {\n    \"units\": \"" +
+                units +
+                "\",\n    \"price\":\"" +
+                limit +
+                "\",\n    \"instrument\": \"" +
+                currencyPair +
+                "\",\n    \"timeInForce\": \"GTC\",\n    \"gtdTime\" :\"" +
+                "2020-06-16 9:48:11 PM" +
+                "\",\n    \"type\": \"LIMIT\",\n    \"takeProfitOnFill\": {\n      \"price\": \"" +
+                takeProfit +
+                "\"\n    },\n    \"stopLossOnFill\": {\n      \"price\": \"" +
+                stopLoss +
+                "\"\n    },\n    \"positionFill\": \"DEFAULT\"\n  }\n}";
             var restClient = new RestClient("https://api-fxpractice.oanda.com/v3/accounts/101-002-14835452-002/orders");
             var restRequest = new RestRequest();
             restRequest.Method = RestSharp.Method.POST;
--- Program.cs
+++ Program.cs
@@ -1,24 +1,26 @@
-﻿using System;
-using TradingAPI.MT4Server;
-using Telegram.Bot;
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text.RegularExpressions;
-using System.Collections.Generic;
+using Telegram.Bot;
 
 namespace testapp
 {
     class Program
     {
         private static readonly TelegramBotClient bot = new TelegramBotClient("1208677836:AAHvCKbmeUZvrFPtg4VFs0DDge161osjcWc");
+
         private static HttpClient client = new HttpClient();
         private static OandaRestClient orc = new OandaRestClient();
-       
+
         static void Main(string[] args)
         {
 
-            
 
-            
+            //orc.PlaceLimitOrder(new List<string>());
+
             Console.WriteLine("Waiting...");
             bot.OnMessage += Bot_OnMessage;
 
@@ -29,7 +31,7 @@ namespace testapp
 
         public static string FilterMessage(string message)
         {
-            
+
             char[] arr = message.ToCharArray();
             arr = Array.FindAll<char>(arr, (c => (char.IsLetterOrDigit(c) || c == '.')));
             var filteredSting = new string(arr).ToUpper();
@@ -39,7 +41,7 @@ namespace testapp
         {
             var filteredSting = FilterMessage(tradeMessage);
             var tradeList = new List<string>();
-            
+
             var tp1Index = filteredSting.IndexOf("TP");
             var tp2Index = filteredSting.IndexOf("TP", tp1Index + 2);
             var tp3Index = filteredSting.IndexOf("TP", tp2Index + 2);
@@ -84,7 +86,7 @@ namespace testapp
             int slCount = Regex.Matches(input, "SL").Count;
             int sellCount = Regex.Matches(input, "SELL").Count;
             int buyCount = Regex.Matches(input, "BUY").Count;
-            if((sellCount==1|| buyCount==1)&& tpCount >=1 && slCount>=1)
+            if ((sellCount == 1 || buyCount == 1) && tpCount >= 1 && slCount >= 1)
             {
                 return true;
             }
@@ -97,21 +99,41 @@ namespace testapp
 
         private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
-            if(e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
             {
                 if (IsTradeData(FilterMessage(e.Message.Text)))
                 {
-                    
-                    var td = ParseTradeMessage(e.Message.Text);
-                    Console.WriteLine("Trade Message"+td[0]+":"+td[1]+":"+td[2]);
-                    orc.PlaceOrder(td);
+                    Console.WriteLine(e.Message.Text);
+                    //var td = ParseTradeMessage(e.Message.Text);
+                    //Console.WriteLine("Trade Message"+td[0]+":"+td[1]+":"+td[2]);
+                    //orc.PlaceOrder(td);
+                    var msg = e.Message.Text;
+
+
+
+                    var restClient = new RestClient("https://tradeapi20200720060309.azurewebsites.net/api/goldcup?message=" + msg);
+                    var restRequest = new RestRequest();
+                    restRequest.Method = RestSharp.Method.GET;
+                    var response = restClient.Execute(restRequest);
+                    HttpStatusCode statusCode = response.StatusCode;
+                    int numericStatusCode = (int)statusCode;
+                    Console.WriteLine(numericStatusCode);
+                    if (numericStatusCode == 200)
+                    {
+                        Console.WriteLine("Message Posted");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Message Posting FAILED!!!");
+
+                    }
 
                 }
                 else
                 {
                     Console.WriteLine("Not a trade message");
                 }
-               
+
             }
         }
     }
